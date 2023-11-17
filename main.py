d1c5f404943486f13a97b3f8baacea2183ec36ce
Zeng-GL <73515978+Zeng-GL@users.noreply.github.com>
2023-11-16 21:10:55 -0600

    update / change yaml file content

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ def get_data_from_database(user_message):
         return result
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
